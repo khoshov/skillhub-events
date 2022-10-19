bea6bec444423885d7869bd436fc5acb97551097
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-10-19 22:59:39 +0300

    init docker

--- config/settings.py
+++ config/settings.py
@@ -9,11 +9,24 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
+import sys
 from pathlib import Path
 
+import environ  # noqa
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Allows to keep applications in apps directory
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+
+# Django-environ
+# https://django-environ.readthedocs.io/en/latest/index.html
+env = environ.Env(DEBUG=(bool, False))
+
+# reading .env file if DEBUG is False
+env.read_env(env.str('ENV_PATH', os.path.join(BASE_DIR, '.env')))
 
 
 # Quick-start development settings - unsuitable for production
@@ -74,12 +87,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': env.db()}
 
 
 # Password validation
